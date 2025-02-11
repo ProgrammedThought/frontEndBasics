69ab3f308e4ef0152dc737c0673ae27686b0189b
Hadi Hafeez <hadihafeez5@gmail.com>
2025-02-10 23:00:01 -0500

    Adding Button component

--- React/First-app/src/App.tsx
+++ React/First-app/src/App.tsx
@@ -1,11 +1,34 @@
+import { useState } from "react";
+import Alert from "./components/Alert";
 import ListGroup from "./components/ListGroup";
+import Button from "./components/button";
 
 function App() {
   let items = ["New York", "San Fransisco", "Tokoyo", "London", "Paris"];
 
+  const handleSelectItem = (item: string, index: number) => {
+    console.log(item);
+    setSelectedIndex(index);
+  };
+
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+
   return (
     <div>
-      <ListGroup items={items} heading="Cities" />
+      <Alert>Hello World</Alert>
+      <ListGroup
+        items={items}
+        heading="Cities"
+        onSelectItem={handleSelectItem}
+      />
+      <Button
+        onClick={() => {
+          console.log("Click");
+        }}
+        colour="danger"
+      >
+        {selectedIndex === -1 ? "Nothing selected" : items[selectedIndex]}
+      </Button>
     </div>
   );
 }
